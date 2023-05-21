1b5865f17a228ba836421815ce6448493bb8518a
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-05-21 03:07:00 +0300

    set google font 'Noto Sans'. Add gloobal variable in 'global.css file'

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import { AppProps } from 'next/dist/shared/lib/router/router';
+import Head from 'next/head';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+	return (
+		<>
+			<Head>
+				<title>page-rating</title>
+				<link rel="icon" href="/favicon.ico" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" />
+				<link
+					rel="stylesheet"
+					href="https://fonts.googleapis.com/css2?family=Finger+Paint&family=Noto+Sans:wght@300;400;500;700&display=swap"
+				/>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 
