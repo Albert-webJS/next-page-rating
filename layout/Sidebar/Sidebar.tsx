3dee4a1dabbfe0e4c1609ced99e3f8e9f6c8cae9
Albert Pivnenko <pivnenko.albert@gmail.com>
2023-06-06 07:43:42 +0300

    add styles for sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,11 +1,18 @@
 import { DetailedHTMLProps, HTMLAttributes } from 'react';
 import { Menu } from '../Menu/Menu';
+import styles from './Sidebar.module.css';
+import cn from 'classnames';
 
 interface SidebarProps extends DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> { }
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
-		<aside {...props}>
+		<aside
+			className={cn(styles.sidebar, className)}
+			{...props}
+		>
+			<div className={styles.logo}>Logo</div>
+			<div>search...</div>
 			<Menu />
 		</aside>
 	);
